ad4e9ed3ab10d2c496d211a8d29ec2546da19765
Emaleigh Adams <emaleigh.a304@stu.nebo.edu>
2018-10-26 08:10:00 -0600

    4 total Particles

--- 2d game/Assets/Scripts/CharacterMove.cs
+++ 2d game/Assets/Scripts/CharacterMove.cs
@@ -65,10 +65,10 @@ public class CharacterMove : MonoBehaviour
 
         //Player flip
         if (GetComponent<Rigidbody2D>().velocity.x > 0)
-            transform.localScale = new Vector3(7.756938f, 7.644139f, 1f);
+            transform.localScale = new Vector3(7f, 7f, 1f);
 
         else if (GetComponent<Rigidbody2D>().velocity.x < 0)
-            transform.localScale = new Vector3(-7.756938f, 7.644139f, 1f);
+            transform.localScale = new Vector3(-7f, 7f, 1f);
     }
 
     public void Jump()
