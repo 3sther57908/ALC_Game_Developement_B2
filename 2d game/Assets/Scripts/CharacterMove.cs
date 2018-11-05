97b7818d38d213bd9bc0a874698129f4da47134f
Emaleigh Adams <emaleigh.a304@stu.nebo.edu>
2018-11-05 10:10:04 -0700

    Added a Fairy

--- 2d game/Assets/Scripts/CharacterMove.cs
+++ 2d game/Assets/Scripts/CharacterMove.cs
@@ -65,10 +65,10 @@ public class CharacterMove : MonoBehaviour
 
         //Player flip
         if (GetComponent<Rigidbody2D>().velocity.x > 0)
-            transform.localScale = new Vector3(7f, 7f, 1f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
 
         else if (GetComponent<Rigidbody2D>().velocity.x < 0)
-            transform.localScale = new Vector3(-7f, 7f, 1f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
     }
 
     public void Jump()
